767e61ce01260673b717797eaf642b9bc1bc1bc3
Ömer <omergurbuz25@hotmail.com>
2023-06-15 04:20:01 +0300

    Tamamlandı

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 9000;
+console.log("hello wor333ld");
+
+server.listen(port, () => {
+  console.log("server is listenin on " + port);
+});
 
 // START YOUR SERVER HERE
